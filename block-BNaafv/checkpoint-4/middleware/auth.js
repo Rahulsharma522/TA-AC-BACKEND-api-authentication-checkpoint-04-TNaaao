19bca602d522fc3011c952888933e22cda918b4a
Rahulsharma522 <sharma.sharma.rahul46@gmail.com>
2022-05-23 13:34:44 +0530

    checkpoint-4

---
+++ block-BNaafv/checkpoint-4/middleware/auth.js
@@ -0,0 +1,30 @@
+let jwt = require('jsonwebtoken');
+
+module.exports = {
+  isLoggedIn: async (req, res, next) => {
+    let token = req.headers.authorization;
+    if (!token) {
+      return res.status(400).json({ error: 'token required' });
+    }
+
+  },
+  isAdmin: async (req, res, next) => {
+    let token = req.headers.authorization;
+    if (!token) {
+      return res.status(400).json({ error: 'token required' });
+    }
+
+    try {
+      let payload = await jwt.verify(token, 'thisissecret');
+      if (!payload.isAdmin) {
+        return res
+          .status(400)
+          .json({ error: 'You have to be loggedin as Admin' });
+      }
+      req.user = payload;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+};
